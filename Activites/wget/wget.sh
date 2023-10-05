77b846ad82f8e8ee3857cb15d8be04a3c6587f6a
Joel Sharbrough <jsharbrough@users.noreply.github.com>
2023-10-05 09:18:35 -0600

    Update wget.sh

--- Activites/wget/wget.sh
+++ Activites/wget/wget.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=wget
 #SBATCH --mail-user=email@address.com
 #SBATCH --mail-type=ALL
-#SBATCH --error=sra.err
-#SBATCH --output=sra.out
+#SBATCH --error=wget.err
+#SBATCH --output=wget.out
 
 wget https://github.com/jsharbrough/NMT_Bioinformatics_2023/blob/main/Activites/wget/foo.tar.gz
